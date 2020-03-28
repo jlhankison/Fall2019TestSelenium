422a8fef2ed2a47415cfbcc0aea641c63755bd73
jlhankison <jlhankison@gmail.com>
2020-03-28 12:18:23 -0500

    pages package created, Login page object created

---
+++ src/test/java/com/automation/pages/LoginPage.java
@@ -0,0 +1,46 @@
+package com.automation.pages;
+
+import com.automation.utilities.ConfigurationReader;
+import com.automation.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    @FindBy(id = "prependedInput")
+    public WebElement username;
+
+    @FindBy(id = "prependedInput2")
+    public WebElement password;
+
+    @FindBy(id = "_submit")
+    public WebElement login;
+
+    @FindBy(linkText = "Forgot your password?")
+    public WebElement forgotPassword;
+
+    public LoginPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+
+    }
+    /**
+     * Method to login, version 1
+     * login as a specific user
+     * @param usernameValue
+     * @param passwordValue
+     */
+    public void login(String usernameValue, String passwordValue){
+        username.sendKeys(usernameValue);
+        password.sendKeys(passwordValue);
+    }
+    /**
+     * Method to login, version #2
+     * login as the default user
+     * Credentials will be retrieved from configuration.properties file
+     */
+    public void login(){
+        username.sendKeys(ConfigurationReader.getProperty("store_manager"));
+        password.sendKeys(ConfigurationReader.getProperty("manager_password"));
+    }
+}
--- src/test/java/com/automation/tests/vytrack/TestBase.java
+++ src/test/java/com/automation/tests/vytrack/TestBase.java
@@ -2,15 +2,23 @@ package com.automation.tests.vytrack;
 
 import com.automation.utilities.ConfigurationReader;
 import com.automation.utilities.Driver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public abstract class TestBase {
+        //This is protected because this is an abstract class, we want this to be visible to all child classes but not anywhere else
+    protected WebDriverWait wait;
+    protected Actions actions;
 
     @BeforeMethod
     public void setup(){
         String URL = ConfigurationReader.getProperty("qa1");
         Driver.getDriver().get(URL);
+        wait = new WebDriverWait(Driver.getDriver(), 15);
+        actions = new Actions(Driver.getDriver());
     }
 
     @AfterMethod
